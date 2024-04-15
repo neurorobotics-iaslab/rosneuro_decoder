714f26c047d8ea093a034ac2625637bfc9a43de9
mirovix <miroljubmihailovic98@gmail.com>
2024-04-16 00:35:20 +0200

    chore: moduling code

--- test/TestGenericDecoder.cpp
+++ test/TestGenericDecoder.cpp
@@ -2,85 +2,83 @@
 #include <gtest/gtest.h>
 
 namespace rosneuro {
-namespace decoder {
+    namespace decoder {
+        class TestGenericDecoder: public GenericDecoder {
+            public:
+                TestGenericDecoder() : GenericDecoder() {}
+                ~TestGenericDecoder() {}
+                bool configure(void) { return true; }
+                Eigen::VectorXf apply(const Eigen::VectorXf& in) { return Eigen::VectorXf::Zero(1); }
+                Eigen::VectorXf getFeatures(const Eigen::MatrixXf& in) { return Eigen::VectorXf::Zero(1); }
+                std::string getPath(void) { return ""; }
+                std::vector<int> getClasses(void) { return std::vector<int>({}); }
+        };
 
-class TestGenericDecoder: public GenericDecoder {
-    public:
-        TestGenericDecoder() : GenericDecoder() {}
-        ~TestGenericDecoder() {}
-        bool configure(void) { return true; }
-        Eigen::VectorXf apply(const Eigen::VectorXf& in) { return Eigen::VectorXf::Zero(1); }
-        Eigen::VectorXf getFeatures(const Eigen::MatrixXf& in) { return Eigen::VectorXf::Zero(1); }
-        std::string path(void) { return ""; }
-        std::vector<int> classes(void) { return std::vector<int>({}); }
-};
+        class TestGenericDecoderSuite : public ::testing::Test {
+            public:
+                TestGenericDecoderSuite() { genericDecoder = new TestGenericDecoder(); }
+                ~TestGenericDecoderSuite() { delete genericDecoder; }
+                TestGenericDecoder* genericDecoder;
+        };
 
-class TestGenericDecoderSuite : public ::testing::Test {
-    public:
-        TestGenericDecoderSuite() { genericDecoder = new TestGenericDecoder(); }
-        ~TestGenericDecoderSuite() { delete genericDecoder; }
-        TestGenericDecoder* genericDecoder;
-        std::string plugin = "rosneuro::decoder::Gaussian";
-};
+        TEST_F(TestGenericDecoderSuite, Constructor) {
+            ASSERT_EQ(genericDecoder->is_configured_, false);
+            ASSERT_EQ(genericDecoder->getName(), "undefined");
+        }
 
-TEST_F(TestGenericDecoderSuite, Constructor) {
-    ASSERT_EQ(genericDecoder->is_configured_, false);
-    ASSERT_EQ(genericDecoder->name(), "undefined");
-}
+        TEST_F(TestGenericDecoderSuite, Path){
+            ASSERT_EQ(genericDecoder->getPath(), "");
+        }
 
-TEST_F(TestGenericDecoderSuite, Path){
-    ASSERT_EQ(genericDecoder->path(), "");
-}
+        TEST_F(TestGenericDecoderSuite, Name){
+            ASSERT_EQ(genericDecoder->getName(), "undefined");
+            genericDecoder->setName("test");
+            ASSERT_EQ(genericDecoder->getName(), "test");
+        }
 
-TEST_F(TestGenericDecoderSuite, Name){
-    ASSERT_EQ(genericDecoder->name(), "undefined");
-    genericDecoder->setname("test");
-    ASSERT_EQ(genericDecoder->name(), "test");
-}
+        TEST_F(TestGenericDecoderSuite, Classes){
+            ASSERT_EQ(genericDecoder->getClasses(), std::vector<int>({}));
+        }
 
-TEST_F(TestGenericDecoderSuite, Classes){
-    ASSERT_EQ(genericDecoder->classes(), std::vector<int>({}));
-}
+        TEST_F(TestGenericDecoderSuite, IsSet){
+            ASSERT_EQ(genericDecoder->isSet(), false);
+        }
 
-TEST_F(TestGenericDecoderSuite, IsSet){
-    ASSERT_EQ(genericDecoder->isSet(), false);
-}
+        TEST_F(TestGenericDecoderSuite, LoadVectorOfVector){
+            std::vector<std::vector<uint32_t>> out;
+            ASSERT_TRUE(genericDecoder->loadVectorOfVector("1 2 3;4 5 6;", out));
+            ASSERT_EQ(out.size(), 2);
+            ASSERT_EQ(out[0].size(), 3);
+            ASSERT_EQ(out[1].size(), 3);
+            ASSERT_EQ(out[0][0], 1);
+            ASSERT_EQ(out[0][1], 2);
+            ASSERT_EQ(out[0][2], 3);
+            ASSERT_EQ(out[1][0], 4);
+            ASSERT_EQ(out[1][1], 5);
+            ASSERT_EQ(out[1][2], 6);
+        }
 
-TEST_F(TestGenericDecoderSuite, LoadVectorOfVector){
-    std::vector<std::vector<uint32_t>> out;
-    ASSERT_TRUE(genericDecoder->load_vectorOfVector("1 2 3;4 5 6;", out));
-    ASSERT_EQ(out.size(), 2);
-    ASSERT_EQ(out[0].size(), 3);
-    ASSERT_EQ(out[1].size(), 3);
-    ASSERT_EQ(out[0][0], 1);
-    ASSERT_EQ(out[0][1], 2);
-    ASSERT_EQ(out[0][2], 3);
-    ASSERT_EQ(out[1][0], 4);
-    ASSERT_EQ(out[1][1], 5);
-    ASSERT_EQ(out[1][2], 6);
-}
+        TEST_F(TestGenericDecoderSuite, LoadEigen){
+            Eigen::MatrixXf out = Eigen::MatrixXf::Zero(3, 3);
+            ASSERT_TRUE(genericDecoder->loadEigen("1 2 3;4 5 6;7 8 9;", out));
+            ASSERT_EQ(out.rows(), 3);
+            ASSERT_EQ(out.cols(), 3);
+            ASSERT_EQ(out(0, 0), 1);
+            ASSERT_EQ(out(0, 1), 2);
+            ASSERT_EQ(out(0, 2), 3);
+            ASSERT_EQ(out(1, 0), 4);
+            ASSERT_EQ(out(1, 1), 5);
+            ASSERT_EQ(out(1, 2), 6);
+        }
 
-TEST_F(TestGenericDecoderSuite, LoadEigen){
-    Eigen::MatrixXf out = Eigen::MatrixXf::Zero(3, 3);
-    ASSERT_TRUE(genericDecoder->load_eigen("1 2 3;4 5 6;7 8 9;", out));
-    ASSERT_EQ(out.rows(), 3);
-    ASSERT_EQ(out.cols(), 3);
-    ASSERT_EQ(out(0, 0), 1);
-    ASSERT_EQ(out(0, 1), 2);
-    ASSERT_EQ(out(0, 2), 3);
-    ASSERT_EQ(out(1, 0), 4);
-    ASSERT_EQ(out(1, 1), 5);
-    ASSERT_EQ(out(1, 2), 6);
-}
+        TEST_F(TestGenericDecoderSuite, LoadEigenFail){
+            Eigen::MatrixXf out;
+            ASSERT_FALSE(genericDecoder->loadEigen("1 2 3;4 5 6;7 8 9", out));
+            out = Eigen::MatrixXf::Zero(3, 3);
+            ASSERT_FALSE(genericDecoder->loadEigen("1 2 3;4 5 6;7 8;", out));
+        }
 
-TEST_F(TestGenericDecoderSuite, LoadEigenFail){
-    Eigen::MatrixXf out;
-    ASSERT_FALSE(genericDecoder->load_eigen("1 2 3;4 5 6;7 8 9", out));
-    out = Eigen::MatrixXf::Zero(3, 3);
-    ASSERT_FALSE(genericDecoder->load_eigen("1 2 3;4 5 6;7 8;", out));
-}
-
-}
+    }
 }
 
 int main(int argc, char **argv) {
